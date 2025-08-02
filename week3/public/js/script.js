c13dda132aa60ae12b2b1668d67544c4f511bbe0
Ronit Balar <ronitb.imscit21@gmail.com>
2025-08-02 22:56:20 +1000

    week3

---
+++ week3/public/js/script.js
@@ -0,0 +1,48 @@
+
+const cardList = [
+{
+title: "Wallpaper 1",
+image: "images/cat.png",
+link: "Wallpaper 2",
+desciption: "Preview of the wallpaper 2"
+},
+{
+title: "Wallpaper 2",
+image: "images/M5 CS.png",
+link: "Wallpaper 3",
+desciption: "Preview of the wallpaper 3"
+}
+]
+const clickMe = () => {
+alert("Thanks for clicking me. Hope you have a great day!")
+}
+const submitForm = () => {
+let formData = {};
+formData.first_name = $('#first_name').val();
+formData.last_name = $('#last_name').val();
+formData.password = $('#password').val();
+formData.email = $('#email').val();
+console.log("Form Data Submitted: ", formData);
+}
+const addCards = (items) => {
+items.forEach(item => {
+let itemToAppend = '<div class="col s4 center-align">'+
+'<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+'</div><div class="card-content">'+
+'<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+'<div class="card-reveal">'+
+'<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+'<p class="card-text">'+item.desciption+'</p>'+
+'</div></div></div>';
+$("#card-section").append(itemToAppend)
+});
+}
+$(document).ready(function(){
+$('.materialboxed').materialbox();
+$('#formSubmit').click(()=>{
+submitForm();
+})
+addCards(cardList);
+$('.modal').modal();
+});
+Advance
